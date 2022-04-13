ca26d431dda61edd2c1bb343051328372274dd18
anderseline <aps222@lehigh.edu>
2022-04-12 23:06:03 -0400

    fix paths

--- run_trades.bat
+++ run_trades.bat
@@ -1,2 +1,2 @@
-"C:\Users\ander\AppData\Local\Programs\Python\Python39.exe" "C:\Users\ander\OneDrive\Desktop\Coding\Congressional-Algo-Trading\Senate_Trades.py"
+"C:\Users\ander\AppData\Local\Programs\Python\Python39\python.exe" "C:\Users\ander\OneDrive\Desktop\Coding\Senate_Trades\Senate_Trades.py"
 pause
