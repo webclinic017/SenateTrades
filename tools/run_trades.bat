8bd1ae14f3680e6f7ec65d1b497c5b266b447284
anderseline <aps222@lehigh.edu>
2022-05-09 13:25:35 -0400

    fix path

--- tools/run_trades.bat
+++ tools/run_trades.bat
@@ -1,2 +1,2 @@
-"C:\Users\ander\AppData\Local\Programs\Python\Python39\python.exe" "C:\Users\ander\OneDrive\Desktop\Coding\Senate_Trades\ScrapeTradesToday.py"
-exit
+"C:\Users\ander\AppData\Local\Programs\Python\Python39\python.exe" "C:\Users\ander\OneDrive\Desktop\Coding\Senate_Trades\run\ScrapeTradesToday.py"
+pause
